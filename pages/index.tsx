38062c53876f37de2c28b44c8de76fb942046f2a
meng <monkeystylle@gmail.com>
2021-09-30 02:38:59 +0800

    2nd commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,8 @@
 import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import ProTip from '../src/ProTip';
-import Link from '../src/Link';
-import Copyright from '../src/Copyright';
+import { Box } from '@mui/material';
 
-export default function Index() {
-  return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Next.js v5-beta with TypeScript example
-        </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
-  );
-}
+const Index = () => {
+  return <Box sx={{ my: 4 }}>Hey ya</Box>;
+};
+
+export default Index;
